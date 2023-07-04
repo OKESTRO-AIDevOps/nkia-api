5a67cabff6144ab33e0c89ad592f4bfa75e3f48c
seantywork <seantywork@gmail.com>
2023-07-04 17:14:31 +0900

    origin json

--- pkg/runtimefs/dotetc_structure.go
+++ pkg/runtimefs/dotetc_structure.go
@@ -1,25 +1,25 @@
 package runtimefs
 
 type AppOrigin struct {
-	RECORDS []RecordInfo
-	REPOS   []RepoInfo
-	REGS    []RegInfo
+	RECORDS []RecordInfo `json:"RECORDS,omitempty"`
+	REPOS   []RepoInfo   `json:"REPOS,omitempty"`
+	REGS    []RegInfo    `json:"REGS,omitempty"`
 }
 
 type RecordInfo struct {
-	NS        string
-	REPO_ADDR string
-	REG_ADDR  string
+	NS        string `json:"NS,omitempty"`
+	REPO_ADDR string `json:"REPO_ADDR,omitempty"`
+	REG_ADDR  string `json:"REG_ADDR,omitempty"`
 }
 
 type RepoInfo struct {
-	REPO_ADDR string
-	REPO_ID   string
-	REPO_PW   string
+	REPO_ADDR string `json:"REPO_ADDR,omitempty"`
+	REPO_ID   string `json:"REPO_ID,omitempty"`
+	REPO_PW   string `json:"REPO_PW,omitempty"`
 }
 
 type RegInfo struct {
-	REG_ADDR string
-	REG_ID   string
-	REG_PW   string
+	REG_ADDR string `json:"REG_ADDR,omitempty"`
+	REG_ID   string `json:"REG_ID,omitempty"`
+	REG_PW   string `json:"REG_PW,omitempty"`
 }
